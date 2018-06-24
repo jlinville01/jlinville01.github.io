3dbb610ed71102138110efb6b283cd1eceb82720
Justin Linville <jlinville01@gmail.com>
2018-06-24 09:21:24 -0700

    adds assets and resume page

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,4 +1,21 @@
 class DemoController < ApplicationController
   def index
+  	render('index')
+  end
+
+  def other_index
+  	redirect_to(:action => 'index')
+  end
+
+  def resume
+  	render('resume')
+  end
+
+  def about
+  	render('about')
+  end
+
+  def contact
+  	render('contact')
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
 	root 'demo#index'
 
 	get 'demo/index'
+	get 'demo/resume', as: :resume
+	get 'demo/contact', as: :contact
+	get 'demo/about', as: :about
 end
