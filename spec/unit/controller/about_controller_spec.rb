9adaaedc1663c6c73cd4ea3ee4116cedf20a4b1a
Justin Linville <justinlinville@awesomenesstv.com>
2018-06-28 21:38:13 -0700

    adds status code test

--- spec/unit/controller/about_controller_spec.rb
+++ spec/unit/controller/about_controller_spec.rb
@@ -1,6 +1,13 @@
 RSpec.describe AboutController, :type => :controller do
-	it "renders the index template" do
-		get :index
-		expect(response).to render_template("index")
-	end
+  describe "Get index" do
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
+
+    it "has a 200 status code" do
+      get :index
+      expect(response.status).to eq(200)
+    end
+  end
 end
